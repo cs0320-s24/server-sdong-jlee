19ac786a315066d1c3319c5b8460baa3eb162853
simd22 <113197504+simd22@users.noreply.github.com>
2024-02-11 13:51:23 -0500

    feat: added functionality to LoadHandler

--- src/main/java/edu/brown/cs/student/main/Server/LoadHandler.java
+++ src/main/java/edu/brown/cs/student/main/Server/LoadHandler.java
@@ -14,7 +14,10 @@ import java.util.Map;
 import java.util.Set;
 
 public class LoadHandler implements Route{
-
+    private CSVState csvState;
+    public LoadHandler(CSVState csvState) {
+        this.csvState = csvState;
+    }
     @Override
     public Object handle(Request request, Response response) throws Exception {
         Set<String> params = request.queryParams();
@@ -25,48 +28,18 @@ public class LoadHandler implements Route{
         // Creates a hashmap to store the results of the request
         Map<String, Object> responseMap = new HashMap<>();
         try {
-            System.out.println("success");
-            // Sends a request to the API and receives JSON back
-            String filepathJson = this.sendRequest();
-
-            // Deserializes JSON into an Activity
-//            Activity activity = ActivityAPIUtilities.deserializeActivity(activityJson);
-
-
-            // Adds results to the responseMap
+            this.csvState.setFileName(filepath);
             responseMap.put("result", "success");
-//            responseMap.put("activity", activity);
-            return responseMap;
 
         } catch (Exception e) {
             e.printStackTrace();
             // This is a relatively unhelpful exception message. An important part of this sprint will be
             // in learning to debug correctly by creating your own informative error messages where Spark
             // falls short.
+
             responseMap.put("result", "Exception");
         }
         return responseMap;
     }
 
-    private String sendRequest() throws IOException, InterruptedException, URISyntaxException {
-        HttpRequest buildBoredApiRequest =
-                HttpRequest.newBuilder()
-                        .uri(new URI("http://www.boredapi.com/api/activity/"))
-                        .GET()
-                        .build();
-
-        // Send that API request then store the response in this variable. Note the generic type.
-        HttpResponse<String> sentBoredApiResponse =
-                HttpClient.newBuilder()
-                        .build()
-                        .send(buildBoredApiRequest, HttpResponse.BodyHandlers.ofString());
-
-        // What's the difference between these two lines? Why do we return the body? What is useful from
-        // the raw response (hint: how can we use the status of response)?
-        System.out.println(sentBoredApiResponse);
-        System.out.println(sentBoredApiResponse.body());
-
-        return sentBoredApiResponse.body();
-
-    }
 }
