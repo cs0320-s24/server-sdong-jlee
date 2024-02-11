549e0c0d2bf8d692a5a8667a10e2484704783947
simd22 <113197504+simd22@users.noreply.github.com>
2024-02-11 18:37:48 -0500

    test

--- src/main/java/edu/brown/cs/student/main/Server/SearchHandler.java
+++ src/main/java/edu/brown/cs/student/main/Server/SearchHandler.java
@@ -1,5 +1,8 @@
 package edu.brown.cs.student.main.Server;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
 import edu.brown.cs.student.main.CreatorInterface.StringCreator;
 import edu.brown.cs.student.main.Parse.CSVParser;
 import edu.brown.cs.student.main.Searcher.Search;
@@ -62,21 +65,43 @@ public class SearchHandler implements Route {
 
     StringCreator stringCreator = new StringCreator();
     String file = this.csvState.getFileName();
-    FileReader freader = new FileReader(file);
+
+    FileReader freader = new FileReader("data/" + file);
+    System.out.println("test past filerader");
+
     CSVParser<String> parser = new CSVParser<>(freader, stringCreator, this.hasHeader);
     Search search = new Search(stringCreator, parser, file);
 
+    List<String> matchingRows;
     if (columnIdentifier == null) {
-      List<String> matchingRows = search.searchFile(searchItem);
+      matchingRows = search.searchFile(searchItem);
+    } else {
+      matchingRows = search.searchFile(searchItem, columnIdentifier, this.hasHeader);
+    }
+    System.out.println(matchingRows);
+
+    if (matchingRows.size() == 0) {
+      responseMap.put("result", "Exception: Search could not find: " + searchItem);
+      return responseMap;
 
     } else {
-      List<String> matchingRows = search.searchFile(searchItem, columnIdentifier, this.hasHeader);
 
+      // as in fromJson, we need to work with ingredients.
+      // The polymorphic factory will automatically _insert_ the "type" field
+      Moshi moshi = new Moshi.Builder().build();
+      // Uses a similar pattern but turns it toJson and returns it as a string
+      JsonAdapter<List<String>> adapter = moshi.adapter(Types.newParameterizedType(List.class, String.class));
+      System.out.println("Test A");
+      responseMap.put("result", adapter.toJson(matchingRows));
     }
 
+    System.out.println("Test B");
 
 //    responseMap.put("result", "search success");
     return responseMap;
   }
 
 }
+
+// /loadcsv?filepath=data/RITownIncome/RI.csv
+// /searchcsv?hasHeader=true&columnIdentifier=City/Town&searchItem=Exeter
