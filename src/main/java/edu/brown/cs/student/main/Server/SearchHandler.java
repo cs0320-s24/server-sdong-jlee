9d7b7fc7154079151e41e49f6e7ef15aab6d988b
jhmlee <jhmlee07@gmail.com>
2024-02-12 20:00:34 -0500

    test

--- src/main/java/edu/brown/cs/student/main/Server/SearchHandler.java
+++ src/main/java/edu/brown/cs/student/main/Server/SearchHandler.java
@@ -132,3 +132,5 @@ public class SearchHandler implements Route {
     }
   }
 
+}
+
--- src/main/java/edu/brown/cs/student/main/Server/ViewHandler.java
+++ src/main/java/edu/brown/cs/student/main/Server/ViewHandler.java
@@ -3,10 +3,104 @@ package edu.brown.cs.student.main.Server;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Moshi.Builder;
+import edu.brown.cs.student.main.CreatorInterface.StringCreator;
+import edu.brown.cs.student.main.Parse.CSVParser;
+import edu.brown.cs.student.main.Searcher.Search;
+import edu.brown.cs.student.main.Server.SearchHandler.SearchSuccessResponse;
+import edu.brown.cs.student.main.Server.SearchHandler.UnableToReadFile;
+import java.util.ArrayList;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ViewHandler implements Route {
+  private CSVState csvState;
+
+  public ViewHandler (CSVState csvState) {
+    this.csvState = csvState;
+  }
+
   @Override
   public Object handle(Request request, Response response) throws Exception {
-    return null;
+    Set<String> params = request.queryParams();
+    System.out.println(params);
+
+    // Creates a hashmap to store the results of the request
+    Map<String, Object> responseMap = new HashMap<>();
+
+    // Check that file is loaded
+    if (this.csvState.fileNameIsEmpty()) {
+      return new FileNotLoadedResponse().serialize();
+    }
+
+    StringCreator stringCreator = new StringCreator();
+    String file = this.csvState.getFileName();
+
+    FileReader freader = null;
+    try {
+      freader = new FileReader("data/" + file);
+    } catch (Exception e) {
+      return new UnableToReadFile().serialize();
+    }
+
+    CSVParser<String> parser = new CSVParser<>(freader, stringCreator, this.csvState.getHasHeader());
+    List<List<String>> viewResult = new ArrayList<>();
+
+    List<String> parsedFile = parser.parseCSV();
+
+
+    for (String row : parsedFile) {
+      List<String> curList = new ArrayList<>();
+      curList.add(row);
+      viewResult.add(curList);
+    }
+    responseMap.put("View Result: ", viewResult);
+    return new ViewSuccessResponse(responseMap).serialize();
+
+    // viewcsv, which sends back the entire CSV file's contents as a Json 2-dimensional array.
+    // parse into list of list<String>
   }
+
+  /** Response object to send, when view is successful */
+  public record ViewSuccessResponse(String response_type, Map<String, Object> responseMap) {
+    public ViewSuccessResponse(Map<String, Object> responseMap) {
+      this("success", responseMap);
+    }
+
+    /** @return this response, serialized as Json */
+    String serialize() {
+      try {
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<ViewHandler.ViewSuccessResponse> adapter = moshi.adapter(
+            ViewHandler.ViewSuccessResponse.class);
+        return adapter.toJson(this);
+      } catch (Exception e) {
+        e.printStackTrace();
+        throw e;
+      }
+    }
+  }
+
+  /** Response object to send, when a file has not been loaded before searching */
+  public record FileNotLoadedResponse(String error) {
+    public FileNotLoadedResponse() {
+      this("Exception: must load a csv file to search");
+    }
+    /** @return this response, serialized as Json */
+    String serialize() {
+      Moshi moshi = new Builder().build();
+      return moshi.adapter(FileNotLoadedResponse.class).toJson(this);
+    }
+  }
+
 }
+
