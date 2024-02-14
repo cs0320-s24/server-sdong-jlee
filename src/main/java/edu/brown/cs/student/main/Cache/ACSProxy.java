c0b08025abf74de7b2b8f045129d35018bc2b4bc
jhmlee <jhmlee07@gmail.com>
2024-02-14 17:38:52 -0500

    refactor: added classes/changed organization

--- src/main/java/edu/brown/cs/student/main/Cache/ACSProxy.java
+++ src/main/java/edu/brown/cs/student/main/Cache/ACSProxy.java
@@ -1,3 +1,20 @@
 package edu.brown.cs.student.main.Cache;
 
-public class ACSProxy {}
+import edu.brown.cs.student.main.Server.BroadbandHandler;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class ACSProxy implements Route {
+
+  private BroadbandHandler broadbandHandler;
+  //TODO add user parameters to constructor
+  public ACSProxy(BroadbandHandler broadbandHandler) {
+    this.broadbandHandler = broadbandHandler;
+  }
+
+  @Override
+  public Object handle(Request request, Response response) throws Exception {
+    return null;
+  }
+}
